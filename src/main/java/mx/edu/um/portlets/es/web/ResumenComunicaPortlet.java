6dee88e18774b7d1d0b7114a6941632ab397edb5
J. David Mendoza <jdmendoza@um.edu.mx>
2011-04-28 08:00:05 -0500

    Agregue el portlet del resumen de comunica

---
+++ src/main/java/mx/edu/um/portlets/es/web/ResumenComunicaPortlet.java
@@ -0,0 +1,137 @@
+package mx.edu.um.portlets.es.web;
+
+import com.liferay.portal.kernel.servlet.ImageServletTokenUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.theme.ThemeDisplay;
+import com.liferay.portlet.asset.model.AssetEntry;
+import com.liferay.portlet.asset.service.AssetEntryServiceUtil;
+import com.liferay.portlet.asset.service.AssetTagLocalServiceUtil;
+import com.liferay.portlet.asset.service.persistence.AssetEntryQuery;
+import com.liferay.portlet.imagegallery.model.IGImage;
+import com.liferay.portlet.imagegallery.service.IGImageLocalServiceUtil;
+import com.liferay.portlet.journal.model.JournalArticle;
+import com.liferay.portlet.journal.service.JournalArticleLocalServiceUtil;
+import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TimeZone;
+import javax.portlet.PortletSession;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+import mx.edu.um.portlets.es.utils.TemaUtil;
+import mx.edu.um.portlets.es.utils.ZonaHorariaUtil;
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+import org.joda.time.Weeks;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author jdmr
+ */
+@Controller
+@RequestMapping("VIEW")
+public class ResumenComunicaPortlet {
+
+    private static final Logger log = LoggerFactory.getLogger(ResumenComunicaPortlet.class);
+
+    public ResumenComunicaPortlet() {
+        log.info("Se ha creado una nueva instancia del portlet de resumen de dialoga");
+    }
+
+    @RequestMapping
+    public String ver(RenderRequest request, RenderResponse response, Model model) {
+        log.debug("Mostrando el resumen");
+        TimeZone tz = null;
+        DateTimeZone zone = null;
+        ThemeDisplay themeDisplay = (ThemeDisplay) request.getAttribute(WebKeys.THEME_DISPLAY);
+        try {
+            tz = themeDisplay.getTimeZone();
+            zone = DateTimeZone.forID(tz.getID());
+        } catch (IllegalArgumentException e) {
+            zone = DateTimeZone.forID(ZonaHorariaUtil.getConvertedId(tz.getID()));
+        }
+        try {
+            long scopeGroupId = themeDisplay.getScopeGroupId();
+
+            AssetEntryQuery assetEntryQuery = new AssetEntryQuery();
+
+            DateTime hoy = (DateTime) request.getPortletSession().getAttribute("hoy", PortletSession.APPLICATION_SCOPE);
+            if (hoy == null) {
+                hoy = new DateTime(zone);
+                log.debug("Subiendo atributo hoy({}) a la sesion", hoy);
+                request.getPortletSession().setAttribute("hoy", hoy, PortletSession.APPLICATION_SCOPE);
+            }
+
+            // Buscando los temas de dialoga de la semana
+            String[] tags = getTags(hoy);
+            tags[3] = "dialoga";
+
+            long[] assetTagIds = AssetTagLocalServiceUtil.getTagIds(scopeGroupId, tags);
+
+            assetEntryQuery.setAllTagIds(assetTagIds);
+
+            List<AssetEntry> results = AssetEntryServiceUtil.getEntries(assetEntryQuery);
+
+            log.debug("Buscando los temas de dialoga");
+            for (AssetEntry asset : results) {
+                if (asset.getClassName().equals(JournalArticle.class.getName())) {
+                    model.addAttribute("tituloTema",asset.getTitle().toUpperCase());
+                    model.addAttribute("contenidoTema",asset.getDescription());
+                    break;
+                }
+            }
+
+        } catch (Exception e) {
+            log.error("No se pudo cargar el contenido", e);
+            throw new RuntimeException("No se pudo cargar el contenido", e);
+        }
+
+        return "resumenDialoga/ver";
+    }
+
+    private String[] getTags(DateTime hoy) {
+        String[] tags = new String[4];
+        DateTime inicio = new DateTime(hoy.getYear(), 3, 26, 0, 0, 0, 0, hoy.getZone());
+        log.debug("HOY: {}", hoy);
+        NumberFormat nf = NumberFormat.getInstance();
+        nf.setMinimumIntegerDigits(2);
+        tags[0] = new Integer(hoy.getYear()).toString();
+        tags[1] = "t2";
+        Weeks weeks = Weeks.weeksBetween(inicio, hoy);
+        tags[2] = "l" + nf.format(weeks.getWeeks() + 1);
+        switch (hoy.getDayOfWeek()) {
+            case 1:
+                tags[3] = "lunes";
+                break;
+            case 2:
+                tags[3] = "martes";
+                break;
+            case 3:
+                tags[3] = "miercoles";
+                break;
+            case 4:
+                tags[3] = "jueves";
+                break;
+            case 5:
+                tags[3] = "viernes";
+                break;
+            case 6:
+                tags[3] = "sabado";
+                break;
+            case 7:
+                tags[3] = "domingo";
+                break;
+        }
+        log.debug("TAGS: {} {} {} {}", tags);
+
+        return tags;
+    }
+}
